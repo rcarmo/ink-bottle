d03653edd3e6d31b5a849edae0f9f53da13eea98
Rui Carmo <rui.carmo@gmail.com>
2014-10-10 17:06:42 +0100

    added base packages

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ sys.path.insert(0,os.path.join(os.path.dirname(os.path.abspath(__file__)),'lib')
 
 from bottle import run
 
-from utils.config import settings
+from config import settings
 
 log = logging.getLogger()
 
---
+++ lib/config.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2012, Rui Carmo
+Description: Shared configuration data
+License: MIT (see LICENSE.md for details)
+"""
+
+import os, sys, platform, logging.config
+from utils.core import get_config, path_for, tb
+
+try:
+    settings
+except NameError:
+    for host in [platform.node(), 'default']:
+        try:
+            settings = get_config(path_for(os.path.join('etc','%s.json' % host)))
+        except IOError:
+            continue
+        except Exception as e:
+            tb = tb()
+            print tb
+            if sys.stderr.isatty():
+                print >> sys.stderr, ("Error while loading %(host)s.json: %(tb)s" % locals())
+            else:
+                log.error("Error while loading %(host)s.json: %(tb)s" % locals())
+            sys.exit(2)
+        logging.config.dictConfig(dict(settings.logging))
+        log = logging.getLogger()
+        log.info("Configuration for %s loaded." % host)
+        break
